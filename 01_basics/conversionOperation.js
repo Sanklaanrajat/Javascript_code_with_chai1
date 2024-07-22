abf2ce6c70e60fffd1596f57dd92890c756d41d4
Rajat Sanklaan <143341424+Sanklaanrajat@users.noreply.github.com>
2024-07-22 10:03:17 +0000

    Conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "Rajat";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+// "33" => 33
+// "3abx" => Nan
+// true = 1 , false = 0
+let isLoggedIn = "Rajat";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1 = true; 0 = false;
+// "" = false
+// "Rajat" => True
+
+let someNumber = 30;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
